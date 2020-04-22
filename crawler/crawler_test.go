9d00a48a17010b86d62c12f5180c9099ca0a99f7
Chris <chris@ob1.io>
2020-04-22 13:21:22 -0400

    Fix CrawlNode test

--- crawler/crawler_test.go
+++ crawler/crawler_test.go
@@ -223,6 +223,11 @@ func TestCrawler_CrawlNode(t *testing.T) {
 
 	defer mn.TearDown()
 
+	sub, err := crawler.Subscribe()
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	eventSub, err := mn.Nodes()[2].SubscribeEvent(&events.PublishFinished{})
 	if err != nil {
 		t.Fatal(err)
@@ -248,9 +253,17 @@ func TestCrawler_CrawlNode(t *testing.T) {
 		t.Fatal("Timed out waiting on publish")
 	}
 
-	sub, err := crawler.Subscribe()
-	if err != nil {
-		t.Fatal(err)
+	select {
+	case obj := <-sub.Out:
+		pro, ok := obj.Data.(*models.Profile)
+		if !ok {
+			t.Fatal("Invalid type assertion")
+		}
+		if pro.Name != profile.Name {
+			t.Fatal("Returned incorrect profile")
+		}
+	case <-time.After(time.Second * 5):
+		t.Fatal("Timed out waiting on subscription")
 	}
 
 	if err := crawler.CrawlNode(mn.Nodes()[2].Identity()); err != nil {
